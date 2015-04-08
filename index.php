81d692983bb04a66d6f7bfa0d6293d9fc705cd5b
Le-Yo <lenykoskey@yahoo.com>
2015-04-08 18:17:56 +0300

    introduction ot php

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+//echo "Hello world";
+
+//declaring variables:
+
+//integer declaration
+//$i = 1;
+//echo $i;
+
+//string
+
+//$name = "Steve";
+
+//echo $name;
+
+//$payment_status = TRUE;
+//echo $payment_status;
+
+//mathematical operations
+
+//logical operations
+$i = 1;
+$j = 2;
+
+$z = $i - $j;
+
+echo $z;
+
+?>
