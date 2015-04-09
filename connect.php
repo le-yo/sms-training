323df447db422a85b6926a21f635beffa87c09dc
Le-Yo <lenykoskey@yahoo.com>
2015-04-09 19:19:31 +0300

    added select db statement

--- connect.php
+++ connect.php
@@ -4,7 +4,7 @@ $hostname = "localhost";
 $username = "leo";//root
 $password = "12345";//""
 $connection =  mysql_connect($hostname, $username, $password) or trigger_error(mysql_error(),E_USER_ERROR);
+mysql_select_db("bima");
 
-print_r($connection);
 
 ?>
