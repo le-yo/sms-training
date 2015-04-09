d180adc3e36957e0b0041c512e49a6a9aff9568a
Le-Yo <lenykoskey@yahoo.com>
2015-04-09 18:47:47 +0300

    added database

---
+++ premiums.php
@@ -0,0 +1,37 @@
+<?php
+
+//kagai's story:
+
+//The normal sales cycle of insurance.
+
+//Agent approaches you with a product and gives you details
+
+//User register by SMS (Name, age, Id). Confirmation message is here
+
+//pay 100 bob per month via MPESA
+
+//Receive confirmation that insurance is active
+
+//5 days to expiry it should send an alert message for renewal
+
+//insured person should query on the status of the insurance. Send STATUS you
+//insurance is active or the next x days.
+
+//Ask questions, feedback, idea to insurance company.
+
+//send a renewal message 5 days before expiry of insurance.
+
+//Nouns
+
+/*
+ * 1. User, message(phone,message,timestamp,type), payment(amount,user,what),
+ *  insurance (status, expiry date), questions(user), fb (user),idea (user)
+ */
+
+//Verbs
+/*
+ * 2. (user) register, (system) sendConfirmation, (user) pays, (system)
+ * sends renewal message
+ *
+ *
+ */
