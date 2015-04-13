fed4f5aeeb4a42c64b3264759787d8b8c3d8ab37
Le-Yo <lenykoskey@yahoo.com>
2015-04-13 19:44:26 +0300

    added africastalking lib and example

--- premiums.php
+++ premiums.php
@@ -1,7 +1,8 @@
 <?php
 include('connect.php');
 //kagai's story:
-
+sendSMS();
+exit;
 //The normal sales cycle of insurance.
 
 //Agent approaches you with a product and gives you details
@@ -177,3 +178,52 @@ function sendOutput($msg,$number){
     echo json_encode($payload);
     exit;
 }
+
+function sendSMS(){
+
+    require_once('AfricasTalkingGateway.php');
+// Specify your login credentials
+    $username   = "username";
+    $apikey     = "password";
+
+// Specify the numbers that you want to send to in a comma-separated list
+// Please ensure you include the country code (+254 for Kenya in this case)
+    $recipients = "+254718931397";
+// And of course we want our recipients to know what we really do
+    $message    = "I'm a lumberjack and its ok, I sleep all night and I work all day";
+// Create a new instance of our awesome gateway class
+    $gateway    = new AfricasTalkingGateway($username, $apikey);
+// Any gateway errors will be captured by our custom Exception class below,
+// so wrap the call in a try-catch block
+    try
+    {
+        // Thats it, hit send and we'll take care of the rest.
+        $results = $gateway->sendMessage($recipients, $message);
+        foreach($results as $result) {
+            // Note that only the Status "Success" means the message was sent
+            echo " Number: " .$result->number;
+            echo " Status: " .$result->status;
+            echo " MessageId: " .$result->messageId;
+            echo " Cost: "   .$result->cost."\n";
+        }
+    }
+    catch ( AfricasTalkingGatewayException $e )
+    {
+        echo "Encountered an error while sending: ".$e->getMessage();
+    }
+// DONE!!!
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
